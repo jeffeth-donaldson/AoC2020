22ffa803945f1c94ae4a9fbc1a27025fa13f4364
Joshua <josh.mcclung66@gmail.com>
2020-12-20 18:29:07 -0700

    Day1 part 2

--- 01/solution.py
+++ 01/solution.py
@@ -1,12 +1,12 @@
 def makeSet(file):
     """Pull the items from the puzzle input and put in a set"""
-    values = set()
+    values = []
     f = open(file)
     lines = f.readlines()
     f.close()
     print(lines)
     for line in lines:
-        values.add(int(line.strip()))
+        values.append(int(line.strip()))
     return values
 
 def findValue(values):
@@ -18,6 +18,13 @@ def findValue(values):
             return value * (2020 - value)
         else:
             print(2020 - value, "not in set")
+def find3Values(values):
+    """Returns the product of three values in set that add to 2020"""
+    for i in range(len(values)):
+        for j in range(i+1,len(values)):
+            if 2020 - (values[i] + values[j]) in values:
+                return values[i] * values[j] * (2020 - values[i] - values[j])
+
 
 def test():
     testset = {1721, 979, 266, 299, 675, 1456}
@@ -26,7 +33,11 @@ def test():
     return findValue(testset)
 if __name__ == "__main__":
     values = makeSet("input")
-    product = findValue(values)
+    option = input("2 or 3")
+    if option == "2":
+        product = findValue(values)
+    elif option == "3":
+        product = find3Values(values)
     # product = test()
     if product != None:
         print("Value:", product)
